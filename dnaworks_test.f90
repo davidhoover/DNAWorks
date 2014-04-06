eb5338e914c9cc07c49832738d66846c42b13d88
David Hoover <hooverdm@helix.nih.gov>
2014-04-06 17:03:37 -0400

    Create dnaworks_test.f90

---
+++ dnaworks_test.f90
@@ -0,0 +1,11 @@
+MODULE dnaworks_test
+
+  IMPLICIT NONE
+  SAVE
+
+  LOGICAL :: TEST0=.FALSE.               ! Print TEST0 messages
+  LOGICAL :: TEST1=.FALSE.               ! Print TEST1 messages
+  LOGICAL :: TEST2=.FALSE.              ! Print TEST2 messages
+  LOGICAL :: TEST3=.FALSE.              ! Print TEST3 messages
+
+END MODULE dnaworks_test
